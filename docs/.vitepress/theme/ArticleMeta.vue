8ac56cc13b4c6536f797bf095231bba067451978
Jacko <l2982211930@163.com>
2026-04-17 22:23:09 +0800

    feat(ArticleMeta): generate article meta base on frontmatter instand of hard-code

---
+++ docs/.vitepress/theme/ArticleMeta.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import { computed } from 'vue'
+    import { useData } from 'vitepress'
+
+
+    const { frontmatter } = useData()
+    const title = computed(() => frontmatter.value.title as string | undefined)
+    const date = computed(() => frontmatter.value.date as string | undefined)
+    const location = computed(() => frontmatter.value.location as string | undefined)
+    const tags = computed(() => frontmatter.value.tags as string | undefined)
+    const hasMeta = computed(() => Boolean(title.value || date.value || location.value || tags.value))
+</script>
+
+<template>
+    <div v-if="hasMeta">
+        <h1 class="article-meta-title" v-if="title">{{ title }}</h1>
+        <div v-if="date">📅 {{ date }}</div>
+        <div v-if="location">📍 {{ location }}</div>
+        <div v-if="tags">🏷️ {{ tags }}</div>
+    </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,15 @@
-import Theme from 'vitepress/theme'
-import './custom.css'
+import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 import 'virtual:group-icons.css'
+import './custom.css'
+import ArticleMeta from './ArticleMeta.vue'
 
 
-export default Theme
+export default {
+	extends: DefaultTheme,
+	Layout() {
+		return h(DefaultTheme.Layout, null, {
+			'doc-before': () => h(ArticleMeta)
+		})
+	}
+}
