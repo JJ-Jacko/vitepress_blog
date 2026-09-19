e54835d4bd47cec8e65620db3b3804619fa78f47
Jacko <l2982211930@163.com>
2026-09-19 22:10:50 +0800

    feat(theme.ArticleMeta.title): load post meta from `constants`

--- .vitepress/theme/ArticleMeta.vue
+++ .vitepress/theme/ArticleMeta.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
-    import { computed } from 'vue'
-    import { useData } from 'vitepress'
+    import { computed } from 'vue';
+    import { useData } from 'vitepress';
+
+    import { getPost } from "../tools";
 
 
-    const { frontmatter } = useData()
-    const title = computed(() => frontmatter.value.title as string | undefined)
-    const date = computed(() => frontmatter.value.date as string | undefined)
-    const location = computed(() => frontmatter.value.location as string | undefined)
-    const tags = computed(() => frontmatter.value.tags as string | undefined)
-    const hasMeta = computed(() => Boolean(title.value || date.value || location.value || tags.value))
+    const { page, lang } = useData();
+    const post = computed(() => {
+        const fileName = page.value.relativePath.split('/').pop();
+        const postID = fileName?.replace(/\.md$/, '') ?? '';
+        return getPost(postID);
+    });
+
+    let title;
+    if (lang.value === "en-US") {
+        title = computed(() => post.value?.nameEN ?? '');
+    }
+    else if (lang.value === "zh-CN") {
+        title = computed(() => post.value?.nameCN ?? '');
+    }
+    else if (lang.value === "zh-HK") {
+        title = computed(() => post.value?.nameHK ?? '');
+    }
+
 </script>
 
 <template>
-    <div v-if="hasMeta">
+    <div v-if="post">
         <h1 class="article-meta-title" v-if="title">{{ title }}</h1>
-        <div v-if="date">📅 {{ date }}</div>
-        <div v-if="location">📍 {{ location }}</div>
-        <div v-if="tags">🏷️ {{ tags }}</div>
     </div>
 </template>
