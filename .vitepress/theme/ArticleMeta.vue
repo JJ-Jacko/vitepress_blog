24ed600a656ae6f86281970e6c9afb9da3f6afa2
Jacko <l2982211930@163.com>
2026-09-20 09:02:13 +0800

    feat(theme.ArticleMeta.location): load post meta from `constants`

--- .vitepress/theme/ArticleMeta.vue
+++ .vitepress/theme/ArticleMeta.vue
@@ -2,7 +2,7 @@
     import { computed } from 'vue';
     import { useData } from 'vitepress';
 
-    import { getPost } from "../tools";
+    import { getPost, getLocation } from "../tools";
 
 
     const { page, lang } = useData();
@@ -13,14 +13,27 @@
     });
 
     let title;
+    let location;
     if (lang.value === "en-US") {
         title = computed(() => post.value?.nameEN ?? '');
+        location = computed(() => {
+            const code = post.value?.location;
+            return code ? getLocation(code)?.nameEN ?? '' : '';
+        });
     }
     else if (lang.value === "zh-CN") {
         title = computed(() => post.value?.nameCN ?? '');
+        location = computed(() => {
+            const code = post.value?.location;
+            return code ? getLocation(code)?.nameCN ?? '' : '';
+        });
     }
     else if (lang.value === "zh-HK") {
         title = computed(() => post.value?.nameHK ?? '');
+        location = computed(() => {
+            const code = post.value?.location;
+            return code ? getLocation(code)?.nameHK ?? '' : '';
+        });
     }
 
     const date = computed(() => post.value?.date?.toLocaleString());
@@ -31,5 +44,6 @@
     <div v-if="post">
         <h1 class="article-meta-title" v-if="title">{{ title }}</h1>
         <div v-if="date">📅 {{ date }}</div>
+        <div v-if="location">📍 {{ location }}</div>
     </div>
 </template>
